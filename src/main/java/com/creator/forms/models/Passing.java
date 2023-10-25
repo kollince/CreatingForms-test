80c6bdf10feba6b2a5285f6e345328e26f1671f4
nikolai_barabanov <mr.kollince@gmail.com>
2023-10-25 22:09:49 +0300

    add Passing in models

---
+++ src/main/java/com/creator/forms/models/Passing.java
@@ -0,0 +1,12 @@
+package com.creator.forms.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Passing {
+    private int id;
+    private int idQuestion;
+    private int number;
+}
